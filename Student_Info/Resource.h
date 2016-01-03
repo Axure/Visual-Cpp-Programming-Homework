44d43885ce253bc922072e056cc6f3eaeb5b398a
Hu Zheng <axurez@vip.qq.com>
2016-01-04 04:52:33 +0800

    Enhance the layout.

--- Student_Info/Resource.h
+++ Student_Info/Resource.h
@@ -14,12 +14,13 @@
 #define IDC_EDIT3                       1002
 #define IDC_EDIT4                       1003
 #define IDC_EDIT5                       1004
-#define IDC_EDIT6                       1006
 #define IDC_LIST2                       1008
 #define IDC_BUTTON1                     1009
 #define IDC_BUTTON2                     1010
 #define IDC_EDIT7                       1011
 #define IDC_EDIT8                       1012
+#define IDC_COMBO2                      1015
+#define IDC_RADIO1                      1016
 
 // Next default values for new objects
 // 
@@ -27,7 +28,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
