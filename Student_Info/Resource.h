981d99a362e41779528bec7981b5fc733b2e7c45
Hu Zheng <axurez@vip.qq.com>
2016-01-04 04:59:01 +0800

    Enhance the layout.

--- Student_Info/Resource.h
+++ Student_Info/Resource.h
@@ -9,18 +9,22 @@
 #define IDD_STUDENT_INFO_DIALOG         102
 #define IDP_SOCKETS_INIT_FAILED         103
 #define IDR_MAINFRAME                   128
-#define IDC_EDIT1                       1000
-#define IDC_EDIT2                       1001
-#define IDC_EDIT3                       1002
-#define IDC_EDIT4                       1003
-#define IDC_EDIT5                       1004
+#define IDC_EDIT_AGE                    1000
+#define IDC_EDIT_NAME                   1002
+#define IDC_EDIT_STUDENT_ID             1003
+#define IDC_EDIT_DEPARTMENT             1004
 #define IDC_LIST2                       1008
 #define IDC_BUTTON1                     1009
 #define IDC_BUTTON2                     1010
 #define IDC_EDIT7                       1011
 #define IDC_EDIT8                       1012
 #define IDC_COMBO2                      1015
-#define IDC_RADIO1                      1016
+#define IDC_COMBO_GENDER                1017
+#define IDC_STATIC_STUDENT_ID           1018
+#define IDC_STATIC_NAME                 1019
+#define IDC_STATIC_GENDER               1020
+#define IDC_STATIC_AGE                  1021
+#define IDC_STATIC_DEPARTMENT           1022
 
 // Next default values for new objects
 // 
@@ -28,7 +32,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        129
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1017
+#define _APS_NEXT_CONTROL_VALUE         1023
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
