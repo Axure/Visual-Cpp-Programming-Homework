eaf36b19729136f5167d181ef7acdce6ce504cad
Hu Zheng <axurez@vip.qq.com>
2016-01-04 02:54:33 +0800

    Change code layout.

--- Test/main.cpp
+++ Test/main.cpp
@@ -1,48 +1,15 @@
 #include <windows.h>
+#include <stdlib.h>
+#include <string.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
+BOOL InitWindowsClass(HINSTANCE hInstance);
+BOOL InitWindows(HINSTANCE hInstance, int nCmdShow);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow) {
-	HWND hwnd;
 	MSG Msg;
-	WNDCLASS wndclass;
-	char lpszClassName[]="´°¿Ú";
-	char lpszTitle[] = "My_Windows";
-	
-	wndclass.style = 0;
-	wndclass.lpfnWndProc = WndProc;
-	wndclass.cbClsExtra = 0;
-	wndclass.cbWndExtra = 0;
-	wndclass.hInstance = hInstance;
-	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
-	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
-	
-	wndclass.lpszMenuName = NULL;
-	wndclass.lpszClassName = lpszClassName;
-	
-	if(!RegisterClass(&wndclass)) {
-		MessageBeep(0);
-		return FALSE;
-	}
-	
-	hwnd = CreateWindow(
-		lpszClassName,
-		lpszTitle,
-		WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		CW_USEDEFAULT,
-		NULL,
-		NULL,
-		hInstance,
-		NULL
-		);
-	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);
-	
+	if (!InitWindowsClass(hInstance)) return FALSE;
+	if (!InitWindows(hInstance, nCmdShow)) return FALSE;
 	while(GetMessage(&Msg, NULL, 0, 0)) {
 		TranslateMessage(&Msg);
 		DispatchMessage(&Msg);
@@ -58,4 +25,52 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
 		return DefWindowProc(hwnd, message, wParam, lParam);
 	}
 	return (0);
+}
+
+BOOL InitWindows(HINSTANCE hInstance, int nCmdShow) {
+	HWND hwnd;
+	char lpszClassName[]="´°¿Ú";
+	char lpszTitle[] = "My_Windows";
+	hwnd = CreateWindow(
+		lpszClassName,
+		lpszTitle,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+	if (!hwnd) return FALSE;
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+	return TRUE;
+}
+
+BOOL InitWindowsClass(HINSTANCE hInstance) {
+	WNDCLASS wndclass;
+	char lpszClassName[]="´°¿Ú";
+	char lpszTitle[] = "My_Windows";
+	
+	wndclass.style = 0;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = lpszClassName;
+
+	if(!RegisterClass(&wndclass)) {
+		MessageBeep(0);
+		return FALSE;
+	} else {
+		return TRUE;
+	}
 }
